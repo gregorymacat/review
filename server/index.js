0fcc7074aee36dccec728204952490200f2ff332
Gregory Macat <gregorymacat@gmail.com>
2021-11-29 09:35:22 -0500

    Server now serving the jsx

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.static(path.join(__dirname, '../dist')));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send();
 });
 
 app.listen(PORT, () => {
